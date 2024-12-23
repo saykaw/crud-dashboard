9a802f1ec299d13df7beaf93ef39353c3e1b18c0
Sayali Kawatkar <131283831+saykaw@users.noreply.github.com>
2024-12-23 21:01:35 +0530

    updated vite config

--- client/vite.config.mjs
+++ client/vite.config.mjs
@@ -1,16 +1,10 @@
-// vite.config.mjs
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',  // Ensure the app is accessible externally
-    port: process.env.PORT || 4173,  // Use Render's assigned port or fallback to 4173
+    host: '0.0.0.0',  
+    port: process.env.PORT || 4173,  
   },
 });
